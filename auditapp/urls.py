bb29bcd6210b546db4f7b09fb3cc51ebd6c976e7
exoticaakhil <akhilanilvly@gmail.com>
2024-04-01 22:50:38 +0530

    completed

--- auditapp/urls.py
+++ auditapp/urls.py
@@ -4,4 +4,10 @@ from. import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('features',views.features,name='features'),
+    path('about',views.about,name='about'),
+    path('contactus',views.contactus,name='contactus'),
+    path('demo',views.demo,name='demo'),
+    path('price',views.price,name='price'),
+    path('login',views.login,name='login'),
+    path('signup',views.signup,name='signup'),
 ]
--- auditapp/views.py
+++ auditapp/views.py
@@ -5,3 +5,15 @@ def index (request):
    return render(request,'index.html')
 def features(request):
    return render(request,'Features.html')
+def about(request):
+   return render(request,'about.html')
+def contactus(request):
+   return render(request,'contactus.html')
+def demo(request):
+   return render(request,'demo.html')
+def price(request):
+   return render(request,'price.html')
+def login(request):
+   return render(request,'login.html')
+def signup(request):
+   return render(request,'signup.html')
